ba5d2aaee86fa575da99cd9127019d2c916c5924
99002649 <71537287+99002649@users.noreply.github.com>
2020-10-31 17:11:41 +0530

    Create Customer.js

---
+++ Customer.js
@@ -0,0 +1,12 @@
+class customer
+{
+constructor(name,email,date,phoneNumber,Amount)
+ {
+ 
+this.name = name;
+this.email = email;
+this.date=date;
+this.phoneNumber=phoneNumber;
+this.Amount=Amount;
+ }
+}
